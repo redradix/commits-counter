dea7f86e5090a27e5905436d1ae34d6ad6fcf35b
Roberto Redradix <roberto@redradix.com>
2022-12-09 19:00:07 +0100

    Initial script

---
+++ commits-counter.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+set -e
+
+REPOS=$(find . -type d -name ".git")
+
+TOTAL_COMMITS=0
+
+for REPO_PATH in $REPOS
+do
+  echo "Entrando en $REPO_PATH"
+  cd "$REPO_PATH"
+
+  echo "Actualizando referencias desde el repositorio remoto"
+  git fetch --all > /dev/null 2>&1 || echo "git fetch falló, puede que falten algunos commits"
+
+  read REPO_COMMITS <<< $(git log --since="2022-01-01" --oneline | wc -l)
+  echo "Commits en el repo: $REPO_COMMITS"
+
+  echo
+
+  TOTAL_COMMITS=$(($TOTAL_COMMITS + $REPO_COMMITS))
+  cd - > /dev/null
+done
+
+echo
+echo "Total de commits contados: $TOTAL_COMMITS"
